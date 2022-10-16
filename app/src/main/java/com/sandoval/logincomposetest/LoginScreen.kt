bf1fcf3c56649d28a4768db47b1f61c02858f26a
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-10-15 20:19:39 -0500

    Add the clickable to the close icon
    - Add the clickable feature to the close icon to close the application

--- app/src/main/java/com/sandoval/logincomposetest/LoginScreen.kt
+++ app/src/main/java/com/sandoval/logincomposetest/LoginScreen.kt
@@ -1,8 +1,10 @@
 package com.sandoval.logincomposetest
 
+import android.app.Activity
 import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -18,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,10 +44,11 @@ fun LoginScreen() {
 
 @Composable
 fun HeaderLogin(modifier: Modifier) {
-	//val activity = LocalContext.current as Activity
+	val activity = LocalContext.current as Activity
 	Icon(
-		imageVector = Icons.Default.Close, contentDescription = "Close app image", modifier
-		//modifier = modifier.clickable { activity.finish() }
+		imageVector = Icons.Default.Close,
+		contentDescription = "Close app image",
+		modifier = modifier.clickable { activity.finish() }
 	)
 }
 
